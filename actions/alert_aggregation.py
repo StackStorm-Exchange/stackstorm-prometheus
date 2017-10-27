93014f9332e1440b2bc45bf5aaa781c12b1dca30
Kirill Izotov <enykeev@stackstorm.com>
2017-10-27 14:55:05 +0700

    Remove deprecated st2actions import

--- actions/alert_aggregation.py
+++ actions/alert_aggregation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 from collections import defaultdict
 
 
--- actions/lib/prometheus.py
+++ actions/lib/prometheus.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
